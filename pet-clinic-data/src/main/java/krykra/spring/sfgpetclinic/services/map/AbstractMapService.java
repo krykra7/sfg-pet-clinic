93fa43b6bed03f485775c726eb798a2f27cabf5f
Krystian Krawczyk <krykra7@gmail.com>
2018-09-17 15:03:17 +0200

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,16 +1,20 @@
 package krykra.spring.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+
+import krykra.spring.sfgpetclinic.model.BaseEntity;
 
 /**
  * Created by kk on 8/27/2018.
  */
-abstract class AbstractMapService<T, ID> {
+abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    private Map<ID, T> map = new HashMap<>();
+    private Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -20,8 +24,13 @@ abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            object.setId(getNextId());
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
@@ -32,5 +41,17 @@ abstract class AbstractMapService<T, ID> {
 
     void deleteById(ID id) {
         map.remove(id);
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
