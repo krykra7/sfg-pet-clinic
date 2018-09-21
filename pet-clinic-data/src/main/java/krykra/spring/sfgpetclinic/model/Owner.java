7a0bd30a5a201b424a8b494d573555fda7ebfe34
Krystian Krawczyk <krykra7@gmail.com>
2018-09-21 15:34:44 +0200

    Added Visit Entity. Closes #26

--- pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package krykra.spring.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by kk on 8/17/2018.
  */
 public class Owner extends Person {
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
