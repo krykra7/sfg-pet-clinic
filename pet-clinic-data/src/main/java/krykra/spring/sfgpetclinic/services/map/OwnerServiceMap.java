47a8196733449cc1e5ac6c7ae0a3a09926799d17
Krystian Krawczyk <krykra7@gmail.com>
2018-09-24 11:12:15 +0200

    Enhanced owners with contact info and pets. Closes #33

--- pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,17 +1,30 @@
 package krykra.spring.sfgpetclinic.services.map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 import krykra.spring.sfgpetclinic.model.Owner;
+import krykra.spring.sfgpetclinic.model.Pet;
 import krykra.spring.sfgpetclinic.services.OwnerService;
+import krykra.spring.sfgpetclinic.services.PetService;
+import krykra.spring.sfgpetclinic.services.PetTypeService;
 
 /**
  * Created by kk on 8/27/2018.
  */
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    @Autowired
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Owner findById(Long id) {
@@ -35,7 +48,28 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet type is required");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/krykra/spring/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/krykra/spring/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 import krykra.spring.sfgpetclinic.model.Owner;
+import krykra.spring.sfgpetclinic.model.Pet;
 import krykra.spring.sfgpetclinic.model.PetType;
 import krykra.spring.sfgpetclinic.model.Vet;
 import krykra.spring.sfgpetclinic.services.OwnerService;
@@ -43,12 +46,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Jakub");
         owner1.setLastName("Kowalski");
+        owner1.setAddress("Adres");
+        owner1.setCity("City");
+        owner1.setTelephone("123456789");
+
+        Pet jakubsPet = new Pet();
+        jakubsPet.setBirthDate(LocalDate.now());
+        jakubsPet.setPetType(savedDogPetType);
+        jakubsPet.setName("Maciuś");
+        jakubsPet.setOwner(owner1);
+        owner1.getPets().add(jakubsPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Michał");
         owner2.setLastName("Wolski");
+        owner2.setAddress("Adres");
+        owner2.setCity("City");
+        owner2.setTelephone("123456789");
+
+        Pet michalsPet = new Pet();
+        michalsPet.setBirthDate(LocalDate.now());
+        michalsPet.setPetType(savedCatPetType);
+        michalsPet.setName("monika");
+        michalsPet.setOwner(owner2);
+        owner2.getPets().add(michalsPet);
 
         ownerService.save(owner2);
 
