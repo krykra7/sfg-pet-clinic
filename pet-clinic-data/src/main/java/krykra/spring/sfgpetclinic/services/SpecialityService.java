64e16191d5fc7fdbed86465dd6d5bbd9e835a249
Krystian Krawczyk <krykra7@gmail.com>
2018-09-24 11:29:37 +0200

    Added service and map service for speciality. Closes #31

---
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package krykra.spring.sfgpetclinic.services;
+
+import krykra.spring.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by kk on 9/24/2018.
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package krykra.spring.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import krykra.spring.sfgpetclinic.model.Speciality;
+import krykra.spring.sfgpetclinic.services.SpecialityService;
+
+/**
+ * Created by kk on 9/24/2018.
+ */
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+}
