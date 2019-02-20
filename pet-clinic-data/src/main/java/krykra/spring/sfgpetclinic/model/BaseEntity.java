fa852b0c118daaaf01e5efed234a501edafee79d
kkrawczyk <krykra7@gmail.com>
2019-02-20 19:57:10 +0100

    Updated base entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/model/BaseEntity.java
@@ -2,11 +2,19 @@ package krykra.spring.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by kk on 8/27/2018.
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
