8e5e06bfe75c4d81e24925b3e1197fd6afb3ed29
kkrawczyk <krykra7@gmail.com>
2019-02-20 20:13:15 +0100

    Updated vet to use JPA. Closes #37

--- pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package krykra.spring.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by kk on 9/21/2018.
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/model/Vet.java
@@ -3,11 +3,24 @@ package krykra.spring.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 /**
  * Created by kk on 8/17/2018.
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
