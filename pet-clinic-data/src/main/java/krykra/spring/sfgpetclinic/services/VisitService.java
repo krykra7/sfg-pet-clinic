0cf8f5fb883dc647d796d3932eb24d210f229559
kkrawczyk <krykra7@gmail.com>
2019-02-21 13:47:58 +0100

    Added Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package krykra.spring.sfgpetclinic.services;
+
+import krykra.spring.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/krykra/spring/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,42 @@
+package krykra.spring.sfgpetclinic.services.map;
+
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import java.util.Set;
+
+import krykra.spring.sfgpetclinic.model.Owner;
+import krykra.spring.sfgpetclinic.model.Visit;
+import krykra.spring.sfgpetclinic.services.OwnerService;
+import krykra.spring.sfgpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+}
